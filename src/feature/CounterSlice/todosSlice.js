ce6c2a889586b035e4372ee91b6179c16822e38a
Pritam_K <Pritdk533@gmail.com>
2024-08-08 10:09:53 +0530

    Redux toolkit practice

---
+++ src/feature/CounterSlice/todosSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const todoSlice = createSlice({
+  name: "TodoSlice",
+  initialState: initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      return state;
+    },
+
+    deleteTodo: (state, action) => {
+      return state;
+    },
+
+    toggleTodo: (state, action) => {
+      return state;
+    },
+
+    editTodo: (state, action) => {
+      return state;
+    },
+  },
+});
